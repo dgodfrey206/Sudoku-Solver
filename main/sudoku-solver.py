b9656d26b53d4fb0ea97adb416982a38d58ffee3
David Godfrey <david.godfrey99@gmail.com>
2022-03-07 01:00:26 -0500

    Update sudoku-solver.py

--- main/sudoku-solver.py
+++ main/sudoku-solver.py
@@ -1,1 +1,22 @@
 import pygame
+from Solver import Solver
+
+window = pg.display.set_mode((screen_width, screen_height))
+pg.display.set_caption('Sudoku Solver')
+clock = pg.time.Clock()
+
+solver = Solver()
+run = True
+while run:
+    clock.tick(60)
+    for event in pg.event.get():
+        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+            run = False
+
+    window.fill(pg.Color('white'))
+
+    if solver.valid and not solver.solved():
+        solver.step()
+
+    solver.draw(window)
+    pg.display.update()
